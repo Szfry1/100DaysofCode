9cb57ff627f50807b742ed1eb0ddca8aabf82d81
Zen-Hack <sethzfry@protonmail.com>
2021-08-07 22:53:20 -0500

    3.1

---
+++ Day3.py
@@ -0,0 +1,11 @@
+#Day three focuses on control flow and and logical operators
+#I will be doing these unguided, since it is pretty beginner level stuff
+
+#The first thing that angela asks for is to determine whether a number is even or odd. I'm going to use a Modulo and a and if statment here
+
+number = int(input("What is your number? "))
+if number % 2 == 0:
+    print("Your Number is an even number.")
+else:
+    print("Your Number is an odd number.")
+
