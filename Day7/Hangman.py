83c09439b550c254557ebd9a1d14f853f14e15b3
Zen-Hack <sethzfry@protonmail.com>
2021-08-11 23:11:53 -0500

    Hangman project complete

---
+++ Day7/Hangman.py
@@ -0,0 +1,107 @@
+# Step 1
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Done
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+chosen_word = random.choice(word_list)
+
+
+# Create an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
+# So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
+display = []
+word_length = len(chosen_word)
+lives = 6
+for _ in chosen_word:
+    display.append("_")
+
+print(f"The word is {chosen_word}")
+end_of_game = False
+
+while not end_of_game:
+    
+    guess = input("Please guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+    
+
+    print(f"{' '.join(display)}")
+    if lives == 0:
+        end_of_game = True
+        print("You lose")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+
+    print(stages[lives])
+# print(letter_check)
